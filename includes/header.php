09f32f1d1e6bdbb0a7681b616d3bf91b11280d6f
mohsinniky <Mohsinraza490490@gmail.com>
2025-06-25 04:43:34 +0500

    Updated

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title . ' - ' : ''; ?>BixiTech Billing System</title>
-    
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
@@ -12,7 +13,7 @@
     <!-- DataTables CSS -->
     <link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.bootstrap5.min.css" rel="stylesheet">
-    
+
     <!-- Custom CSS -->
     <link href="assets/css/main.css" rel="stylesheet">
     <link href="assets/css/components.css" rel="stylesheet">
@@ -22,17 +23,19 @@
     <link href="assets/css/time-tracking.css" rel="stylesheet">
     <link href="assets/css/team.css" rel="stylesheet">
 </head>
+
 <body>
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container-fluid">
+            <button class="navbar-toggler" type="button" id="sidebarToggle">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <a class="navbar-brand" href="index.php">
                 <img src="assets/images/logo.png" alt="BixiTech" class="brand-logo">
                 BixiTech Billing
             </a>
-            <button class="navbar-toggler" type="button" id="sidebarToggle">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+
             <div class="navbar-nav ms-auto">
                 <div class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
@@ -42,8 +45,11 @@
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="profile.php"><i class="bi bi-person"></i> Profile</a></li>
                         <li><a class="dropdown-item" href="settings.php"><i class="bi bi-gear"></i> Settings</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="logout.php"><i class="bi bi-box-arrow-right"></i> Logout</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="logout.php"><i class="bi bi-box-arrow-right"></i> Logout</a>
+                        </li>
                     </ul>
                 </div>
             </div>
