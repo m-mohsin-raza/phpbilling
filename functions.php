4f0949d6d43008fedd7528c8cfa15d9a58f9696b
mohsinniky <Mohsinraza490490@gmail.com>
2025-06-24 22:08:59 +0500

    db config done

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+// Load Configuration File
+include_once('config/config.php');
+include_once('config/db.class.php');
+include_once('config/db_config.php');
+// include_once'includes/general_functions.php');
+// include_once('includes/business_functions.php');
+// include_once('includes/security.php');
+// include_once('includes/login_functions.php');
+
+// // Switch language
+// if (isset($_GET['lang'])) {
+//     $lang = $_GET['lang'];
+//     if (in_array($lang, ['en', 'es'])) { // Supported languages
+//         $_SESSION['lang'] = $lang;
+//     }
+// }
